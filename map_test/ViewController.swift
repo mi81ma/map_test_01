d2922fee4e498d360eba74c4972d2bb174c6af54
mi81ma <mi81ma@gmail.com>
2019-03-03 11:41:03 +0800

    Initial Commit

---
+++ map_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  map_test
+//
+//  Created by masato on 3/3/2019.
+//  Copyright © 2019 masato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
